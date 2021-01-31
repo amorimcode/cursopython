a1bcd3c62d3416bc5dbd30cfd3ecc0a4733210b8
Bruno <bruno.amorim032@gmail.com>
2021-01-30 22:48:05 -0300

    30-jan

---
+++ conversordetemperaturas.py
@@ -0,0 +1,3 @@
+celsius = float(input('Informe a temperatura em ºC'))
+fahrenheit = (celsius * 9/5) + 32
+print(f'A temperatura é de {fahrenheit:.2f}')
--- produto.py
+++ produto.py
@@ -1,2 +1,3 @@
 preco = float(input('Qual é o preço do produto? R$'))
-novopreco = preco (preco * 5 / 100)
+novopreco = preco - (preco * 5 / 100)
+print(f'O produto que custava R${preco:.2f}, na promação com desconto de 5% vai custar R${novopreco:.2f}')
